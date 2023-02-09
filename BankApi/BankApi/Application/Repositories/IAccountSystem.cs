b8d48510b2a5fa2e31a113184c24003ef1547a3c
Bassant Abdalaziz <37834407+BassantAbdelaziz@users.noreply.github.com>
2023-02-09 21:19:25 +0200

    Update IAccountSystem.cs

--- BankApi/BankApi/Application/Repositories/IAccountSystem.cs
+++ BankApi/BankApi/Application/Repositories/IAccountSystem.cs
@@ -4,6 +4,10 @@ namespace BankApi.Repositories;
 
 public interface IAccountSystem
 {
-
+/// <summary>
+/// Creats a new account of an exsit customer.
+/// </summary>
+/// <param name="request">An object to hold the required data to create new account.</param>
+/// <returns>A account object.</returns>
 Account CreateNewAccount(CustomerAccount request);
 }

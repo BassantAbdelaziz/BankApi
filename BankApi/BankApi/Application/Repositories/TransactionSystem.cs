6520b0acaaff2c9358728824c704f2dec159d835
Bassant Abdalaziz <37834407+BassantAbdelaziz@users.noreply.github.com>
2023-02-09 21:09:04 +0200

    Update TransactionSystem.cs

--- BankApi/BankApi/Application/Repositories/TransactionSystem.cs
+++ BankApi/BankApi/Application/Repositories/TransactionSystem.cs
@@ -4,13 +4,20 @@ namespace BankApi.Repositories;
 
 public class TransactionSystem : ITransactionSystem
 {
-    public Transaction HandleFirstTransaction(double initialCredit, long customer)
+
+    /// <summary>
+    /// creates first transaction in the new opened account.
+    /// </summary>
+    /// <param name="initialCredit">Initial amount to open new account.</param>
+    /// <param name="accountNumber">The account number.</param>
+    /// <returns>A first transaction on the account.</returns>
+    public Transaction CreateFirstTransaction(double initialCredit, long accountNumber)
     {
         var transaction = new Transaction();
 
         transaction.TransactionId = 1;
         transaction.TransactionDateTime = DateTime.Now;
-        transaction.TransactionAccountNumber = customer;
+        transaction.TransactionAccountNumber = accountNumber;
         transaction.AmountAdded = initialCredit;
         transaction.AmountDeducted = 0;
         return transaction;

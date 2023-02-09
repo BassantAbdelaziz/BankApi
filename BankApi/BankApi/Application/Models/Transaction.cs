9a0729823e197f979f746463015b83c421089236
Bassant Abdalaziz <37834407+BassantAbdelaziz@users.noreply.github.com>
2023-02-09 20:59:48 +0200

    Update Transaction.cs

--- BankApi/BankApi/Application/Models/Transaction.cs
+++ BankApi/BankApi/Application/Models/Transaction.cs
@@ -3,30 +3,32 @@ namespace BankApi.Models;
 public class Transaction
 {
     /// <summary>
-    /// Customer-specific identity.
+    /// Transaction-specific identity.
     /// </summary>
     /// <example>"1"</example>
     public int TransactionId { get; set; } 
     
     /// <summary>
-    /// Name of the customer.
+    /// Creation date of the transaction.
     /// </summary>
     /// <example>"Ali"</example>
     public DateTime TransactionDateTime { get; set; }
     
     /// <summary>
-    /// The initial amount of credit.
+    /// The account number.
     /// </summary>
-    /// <example>"20.000"</example>
+    /// <example>"10007891"</example>
     public long TransactionAccountNumber { get; set; }
     
     /// <summary>
-    /// 
+    /// The total amount added
     /// </summary>
+    /// <example>"50.00"</example>
     public double AmountAdded { get; set; }
     /// <summary>
-    ///
+    ///The total amount deducted
     /// </summary>
+    /// <example>"50.00"</example>
     public double AmountDeducted { get; set; }
     
 }

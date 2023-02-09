335f1c12e20cb5993f569fe9294c85ee5b035752
Bassant Abdalaziz <37834407+BassantAbdelaziz@users.noreply.github.com>
2023-02-09 21:05:18 +0200

    Update ITransactionSystem.cs

--- BankApi/BankApi/Application/Repositories/ITransactionSystem.cs
+++ BankApi/BankApi/Application/Repositories/ITransactionSystem.cs
@@ -4,5 +4,11 @@ namespace BankApi.Repositories;
 
 public interface ITransactionSystem
 {
+    /// <summary>
+    /// Handles first transaction in the new opened.
+    /// </summary>
+    /// <param name="initialCredit">Initial amount to open new account.</param>
+    /// <returns>A first transaction on the account.</returns>
+  
     public Transaction HandleFirstTransaction(double initialCredit, long customer);
 }

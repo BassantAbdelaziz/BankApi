930619c1b2484376075ed5c36f97608ce4e6aaa6
Bassant Abdalaziz <37834407+BassantAbdelaziz@users.noreply.github.com>
2023-02-09 20:32:43 +0200

    Update ICustomerSystem.cs

--- BankApi/BankApi/Application/Repositories/ICustomerSystem.cs
+++ BankApi/BankApi/Application/Repositories/ICustomerSystem.cs
@@ -5,5 +5,5 @@ namespace BankApi.Repositories;
 public interface ICustomerSystem
 {
     Customer GetCustomerById(long customerId);
-    List<Customer> read(long customerId);
+    Customer readCustomerFromJsonFile(long customerId);
 }

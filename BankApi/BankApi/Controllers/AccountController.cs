2c983cf4c6d738c0cd633621026d98f1ccaa3987
Bassant Abdalaziz <37834407+BassantAbdelaziz@users.noreply.github.com>
2023-02-09 20:51:36 +0200

    Update AccountController.cs

--- BankApi/BankApi/Controllers/AccountController.cs
+++ BankApi/BankApi/Controllers/AccountController.cs
@@ -5,18 +5,33 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BankApi.Controllers;
 
+/// <summary>
+/// Account controller.
+/// </summary>
 [ApiController]
 [Route("[controller]")]
 public class AccountController : ControllerBase
 {
     private readonly ILogger<AccountController> _logger;
     private readonly IAccountSystem _system;
-
+    /// <summary>
+    /// Constructs a AccountController object.
+    /// </summary>
+    /// <param name="logger">A logger object.</param>
+    /// <param name="system">A account system.</param>
     public AccountController(ILogger<AccountController> logger, IAccountSystem system)
     {
         _logger = logger;
         _system = system;
     }
+    
+    /// <summary>
+    /// Creates a Account object.
+    /// </summary>
+    /// <param name="account">A customer account object.</param>
+    /// <returns>A account object.</returns>
+    /// <response code="200">Returns a customer object.</response>
+    /// <response code="400">If invalid data pass.</response>
     [AllowAnonymous]
     [HttpPost]
     [ProducesResponseType(typeof(CustomerAccount), StatusCodes.Status201Created)]
